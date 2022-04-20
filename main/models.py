2f557956e1cdea3a340159e2d56629678e3291e1
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-20 15:42:50 +0300

    added a method to save users

--- main/models.py
+++ main/models.py
@@ -10,3 +10,14 @@ class User(models.Model):
     email = models.EmailField(max_length=100)
     is_customer = models.BooleanField(default=False)
     is_mover = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.username}User'
+
+    def save_user(self):
+        super().save()
+
+    @classmethod
+    def get_user(cls):
+        user=User.objects.all()
+        return user

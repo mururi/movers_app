3bf463906d45fea7992f068a51ab3d9ab18f7e9a
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-20 19:52:41 +0300

    changed User model to inherit from AbstractUser instead of model.Model class

--- main/models.py
+++ main/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
+class User(AbstractUser):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=255)
     email = models.EmailField(max_length=100)
@@ -24,4 +25,3 @@ class User(models.Model):
 
     def delete_user(self):
         self.delete()
-        

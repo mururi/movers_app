025d198b4c57f441f7b5fcd022bfb1a7758106c9
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-21 14:22:11 +0300

    Edit booking model

--- main/models.py
+++ main/models.py
@@ -72,19 +72,20 @@ class Mover(models.Model):
         instance.move.save()  
         
 class Booking(models.Model):
-    class HouseTypes(models.TextChoices):
-        BEDSITTER = 'BS', _('Bedsitter')
-        ONEBEDROOM = 'OB', _('Onebedroom')
-        TWOBEDROOM = 'TB', _('Twobedroom')
-        STUDIO = 'SD', _('Studio')
-
+    HOUSE_TYPES = [
+        ('BS', 'Bedsitter'),
+        ('OB', 'Onebedroom'),
+        ('TB', 'Twobedroom'),
+        ('SD', 'Studio'),
+    ]
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     vacation_date = models.DateTimeField()
     booking_date = models.DateTimeField(auto_now=True)
     location = models.CharField(max_length=50)
     new_location = models.CharField(max_length=50)
     description = models.TextField()
-    house = models.CharField(max_length=2, choices=HouseTypes.choices)
+    house = models.CharField(max_length=2, choices=HOUSE_TYPES)
     mover = models.ManyToManyField(User)
 
     def __str__(self):

43c0462afb069635eb2b4e9a48ce2a75ae91aaab
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-22 01:45:26 +0300

    debugging

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile, Mover, User
+from .models import Profile, Mover, User, Booking
 
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Mover)
 admin.site.register(User)
+admin.site.register(Booking)

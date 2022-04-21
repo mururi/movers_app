024ba6913bcbfd2d4aa93079bb0850151e1e57d4
KK <karanjakelvin77@gmail.com>
2022-04-21 18:30:50 +0300

    It will import the APIView

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from rest_framework.views import APICiew
 # Create your views here.
 @login_required(login_url='login')
 def index(request):

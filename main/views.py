d44877221b93904eae62f883d9ce76b8b7573817
KK <karanjakelvin77@gmail.com>
2022-04-21 19:10:16 +0300

    It will update the viw function

--- main/views.py
+++ main/views.py
@@ -2,8 +2,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from rest_framework.views import APICiew
+from rest_framework.views import APIView
+
+
 # Create your views here.
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
+# Create a Registeration view
+class RegisterView(APIView):
+    def post(self, request):
+        pass

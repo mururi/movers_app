07ebb55cd8bedb4b26ad8a926aef216234048444
Marial Clarie <marialclarie@gmail.com>
2022-04-21 20:35:46 +0300

    made changes on index.html

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 # Create your views here.
-@login_required(login_url='login')
+
 def index(request):
     return render(request, 'index.html')
+

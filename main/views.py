258f4ba069cc08591000c7a5259c35fea657d97d
12moi <12moi>
2022-04-21 14:31:44 +0300

    add profile view methods

--- main/views.py
+++ main/views.py
@@ -1,8 +1,27 @@
 
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from .forms import UpdateProfileForm
+
 # Create your views here.
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
+
+
+def profile(request, username):
+    return render(request, 'profile.html')
+
+
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile', user.username)
+    else:
+        form = UpdateProfileForm(instance=request.user.profile)
+    return render(request, 'editprofile.html', {'form': form})

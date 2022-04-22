09ec324a43478d384e71cbef164ec56c0c748ed7
KK <karanjakelvin77@gmail.com>
2022-04-22 05:51:10 +0300

    It will provide urls for emails

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,18 @@
+from atexit import register
 from django.urls import path, include
 from . import views
-from .views import RegisterView
+from .views import RegisterView, LoginView, UserView, LogoutView, VerifyEmailView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('make-booking/', views.make_booking, name='make-booking'),
-    path('register', RegisterView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('user/', UserView.as_view(), name='user'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('email_verify/', VerifyEmailView.as_view(), name='email_verify'),
 ]
 
 if settings.DEBUG:

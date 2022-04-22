087600006efd35389a3cceccdbd635f0b3c35760
KK <karanjakelvin77@gmail.com>
2022-04-22 06:10:37 +0300

    It will update files

--- main/urls.py
+++ main/urls.py
@@ -8,7 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name='index'),
     path('make-booking/', views.make_booking, name='make-booking'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('user/', UserView.as_view(), name='user'),
     path('logout/', LogoutView.as_view(), name='logout'),
